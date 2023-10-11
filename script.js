6652fcbe23bb92e6440b5461edfabd871f016faf
cediackermann <ackermanncedi@gmail.com>
2023-10-11 16:42:06 +0200

    logic

--- script.js
+++ script.js
@@ -25,6 +25,62 @@ function Umfang() {
   document.getElementById("umfangOutput").textContent = umfangVonRadius;
 }
 
+function Radius() {
+  let durchmesser = document.getElementById("durchmesserRadiusInput").value;
+
+  durchmesser = parseFloat(durchmesser);
+  console.log(durchmesser);
+
+  let radiusVonDurchmesser = (durchmesser / 2).toFixed(2);
+  console.log(radiusVonDurchmesser);
+
+  document.getElementById("radiusOutput").textContent = radiusVonDurchmesser;
+}
+
+
+function Volumen() {
+  let radius = document.getElementById("radiusVolumenInput").value;
+  let hoehe = document.getElementById("hoeheVolumenInput").value;
+
+  radius = parseFloat(radius);
+  console.log(radius);
+
+  hoehe = parseFloat(hoehe);
+  console.log(hoehe);
+
+  let volumen = (Math.PI * radius * radius * hoehe).toFixed(2);
+  console.log(volumen);
+
+  document.getElementById("volumenOutput").textContent = volumen;
+}
+
+function Grundflaeche() {
+  let radius = document.getElementById("radiusGrundflaecheInput").value;
+
+  radius = parseFloat(radius);
+  console.log(radius);
+
+  let grundflaeche = (Math.PI * radius * radius).toFixed(2);
+  console.log(grundflaeche);
+
+  document.getElementById("grundflaecheOutput").textContent = grundflaeche;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function Pythagoras() {
   let katheteB = document.getElementById("KathetebInput").value;
   let hypothenuseC = document.getElementById("HypothenusecInput").value;
