4694f37016983b57dd08e5032280c430710f0a63
cediackermann <ackermanncedi@gmail.com>
2023-10-23 10:41:28 +0200

    Seiten added to Dreiecke

--- Scripts/Dreieck.js
+++ Scripts/Dreieck.js
@@ -1,1 +1,26 @@
-function 
+function SeiteaSeitebSeitec() {
+    let b = document.getElementById("seiteaSeitebInput").value;
+    b = parseFloat(b);
+    let c = document.getElementById("seiteaSeitecInput").value;
+    c = parseFloat(c);
+    let a = Math.sqrt((c * c) - (b * b));
+    document.getElementById("seiteaOutput0").textContent = a.toFixed(2);
+}
+
+function SeitebSeiteaSeitec() {
+    let a = document.getElementById("seitebSeiteaInput").value;
+    a = parseFloat(a);
+    let c = document.getElementById("seitebSeitecInput").value;
+    c = parseFloat(c);
+    let b = Math.sqrt((a * a) - (c * c));
+    document.getElementById("seitebOutput0").textContent = b.toFixed(2);
+}
+
+function SeitecSeiteaSeiteb() {
+    let a = document.getElementById("seitecSeiteaInput").value;
+    a = parseFloat(a);
+    let b = document.getElementById("seitecSeitebInput").value;
+    b = parseFloat(b);
+    let c = Math.sqrt((a * a) - (b * b));
+    document.getElementById("seitecOutput0").textContent = c.toFixed(2);
+}
