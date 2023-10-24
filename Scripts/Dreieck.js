554071a872f19f0b8a57decf56a2770d5dee122f
cediackermann <ackermanncedi@gmail.com>
2023-10-24 15:24:39 +0200

    Added Rhombus formulas

--- Scripts/Dreieck.js
+++ Scripts/Dreieck.js
@@ -12,7 +12,7 @@ function SeitebSeiteaSeitec() {
     a = parseFloat(a);
     let c = document.getElementById("seitebSeitecInput").value;
     c = parseFloat(c);
-    let b = Math.sqrt((a * a) - (c * c));
+    let b = Math.sqrt((c * c) - (a * a));
     document.getElementById("seitebOutput0").textContent = b.toFixed(2);
 }
 
@@ -21,6 +21,26 @@ function SeitecSeiteaSeiteb() {
     a = parseFloat(a);
     let b = document.getElementById("seitecSeitebInput").value;
     b = parseFloat(b);
-    let c = Math.sqrt((a * a) - (b * b));
+    let c = Math.sqrt((a * a) + (b * b));
     document.getElementById("seitecOutput0").textContent = c.toFixed(2);
+}
+
+function FlaecheSeiteHoehe() {
+    let a = document.getElementById("flaecheSeiteInput").value;
+    a = parseFloat(a);
+    let h = document.getElementById("flaecheHoeheInput").value;
+    h = parseFloat(h);
+    let A = (a * h) / 2;
+    document.getElementById("flaecheOutput0").textContent = A.toFixed(2);
+}
+
+function UmfangSeiteaSeitebSeitec() {
+    let a = document.getElementById("umfangSeiteaInput").value;
+    a = parseFloat(a);
+    let b = document.getElementById("umfangSeitebInput").value;
+    b = parseFloat(b);
+    let c = document.getElementById("umfangSeitecInput").value;
+    c = parseFloat(c);
+    let U = a + b + c;
+    document.getElementById("umfangOutput0").textContent = U.toFixed(2);
 }
